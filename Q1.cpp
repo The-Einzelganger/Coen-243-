bebc4f537d31d40a14f689ca3b0ecbcea919ac5b
The-Einzelganger <59716910+The-Einzelganger@users.noreply.github.com>
2020-04-08 19:50:28 -0400

    added median but handling exemption error

--- Q1.cpp
+++ Q1.cpp
@@ -1,77 +1,124 @@
-#include <iostream>
+//
+//  main.cpp
+//  Assign 5
+//
+//  Created by Timothy Morvan on 08/04/2020.
+//  Copyright © 2020 Timothy Morvan. All rights reserved.
+//
 
-int main() 
+#include <iostream>
+#include <algorithm>
+
+int main()
 {
-    int n;
-    
+    int n = 0;
+
     do
     {
-        try 
+        try
         {
             std::cout << "Enter a number to be used as the dimension of the square matrix: ";
             std::cin >> n;
-
             if (n <= 0)
                 throw "Invalid input, this has to be a positive number!";
-
         }
-
-        catch(const char* str) 
+        catch(const char* str)
         {
             std::cout << "Exception: " << str << std::endl;
         }
-
     }while(n <= 0);
     
-    int * arr = new int [n]; 
+    int * arr = new int [n];
+    int SortArray[n];
 
     for(int x = 0; x < n; x++)
     {
-        for (int y = 0; y < n; ++y) 
+        static int count = 0;
+        
+        for (int y = 0; y < n; ++y)
         {
             do
             {
-                try 
+                try
                 {
                     std::cout << "Enter the value for this index " << "[" << x << "]" << "[" << y << "]" << " of the 2D array: ";
                     std::cin >> * (arr + x * n + y); // pseudo 2D array
-
+                    SortArray[count] = (*(arr + x * n + y));
                     if( * (arr + x * n + y) <= 0)
                         throw "Invalid, this has to be a positive number!";
                 }
-
-                catch(const char* str) 
+                catch(const char* str)
                 {
                     std::cout << "Exception: " << str << std::endl;
-                }       
-                
+                }
+
             }while( * (arr + x * n + y) <= 0);
-            }
+            
+            count++;
         }
-        
+    }
+
+//            int sum = 0;
+//            float average = 0;
+//            int maximum = * arr;
+//            int minimum = * arr;
+//
+//            for (int i = 0; i < counter;i++)
+//            {
+//                    if(* (arr + i) > maximum)
+//                        maximum = *(arr + i);
+//
+//                    if(* (arr + i) < minimum)
+//                        minimum = *(arr + i);
         int sum = 0;
         float average = 0;
         int maximum = * arr;
         int minimum = * arr;
+        float median = 0;
 
+//                    sum += *(arr + i);
+//            }
+//
+//            average = (float)sum / (float)counter;
+    
         for (int i = 0; i < n * n;i++)
         {
-                if(* (arr + i) > maximum)  
+                if(* (arr + i) > maximum)
                     maximum = *(arr + i);
 
+//            std::cout << "The current maximum value is " << maximum << std::endl;
+//            std::cout << "The current minimum value is " << minimum << std::endl;
+//            std::cout << "The sum of the current values is " << sum << std::endl;
+//            std::cout << "The average of the current values is " << average << std::endl;
+            
                 if(* (arr + i) < minimum)
                     minimum = *(arr + i);
 
+//            counter+=1;
                 sum += *(arr + i);
         }
-        
-        average = (float)sum / (float)n;
+//    }
 
+        average = (float)sum / (float)(n*n);
+
+    
+    std::sort(SortArray, SortArray+(n*n));
+    
+    for (int i = 0; i < n*n; i++)
+    std::cout << SortArray[i] << " ";
+    std::cout << "\n";
+    
+    if (n % 2 == 0){
+        median = (float)(SortArray[(n*n)/2] + SortArray[(n*n)/2-1])/2;
+    }
+    else {
+        median = SortArray[(n*n)/2];
+    }
         std::cout << "The current maximum value is " << maximum << std::endl;
         std::cout << "The current minimum value is " << minimum << std::endl;
         std::cout << "The sum of the current values is " << sum << std::endl;
-        std::cout << "The average of the current values is " << average << std::endl;   
-
+        std::cout << "The average of the current values is " << average << std::endl;
+        std::cout << "The median value of this sequence when ordered is " << median << std::endl;
         delete [] arr;
 
     return 0;
